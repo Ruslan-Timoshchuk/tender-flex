573c63dbacf20f91bb0bcb5e78932d2a4fd479c7
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2025-12-18 00:25:07 +0100

    some adjustments

--- src/main/java/com/flex/tender/TenderFlex.java
+++ src/main/java/com/flex/tender/TenderFlex.java
@@ -1,13 +1,13 @@
-package pl.com.tenderflex;
+package com.flex.tender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class TenderFlex {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(TenderFlex.class, args);
     }
     
 }
---
+++ src/main/java/com/flex/tender/constants/SecuredAuthorityNames.java
@@ -0,0 +1,12 @@
+package com.flex.tender.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class SecuredAuthorityNames {
+    
+    public final String CONTRACTOR = "CONTRACTOR";
+    public final String BIDDER = "BIDDER";
+    public final String ADMIN = "ADMIN";
+
+}
