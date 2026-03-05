9a10cf35864a1cc47a7734d47bd14057eea275b1
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-03-05 23:52:29 +0100

    some adjustments

---
+++ src/main/java/com/flex/tender/model/constants/CookieNames.java
@@ -0,0 +1,10 @@
+package com.flex.tender.model.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CookieNames {
+
+    public final String ACCESS_TOKEN = "access-token";
+    
+}
