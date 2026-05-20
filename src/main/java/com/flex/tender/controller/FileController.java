f053d278ce1c1a2f320db1cdfacf71e9d2569141
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-05-20 13:19:45 +0200

    adjustments in files model

--- src/main/java/com/flex/tender/controller/FileController.java
+++ src/main/java/com/flex/tender/controller/FileController.java
@@ -4,6 +4,7 @@ import static com.flex.tender.controller.constant.SecuredAuthorities.*;
 import static org.springframework.http.MediaType.*;
 import java.io.IOException;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,31 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.flex.tender.exception.FileNotExistsException;
 import com.flex.tender.payload.response.FileMetadataResponse;
 import com.flex.tender.service.FileStorageService;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/v1/files")
+@RequestMapping("${api.base.path}/${api.v1}/${api.files.path}")
 @RequiredArgsConstructor
 public class FileController {
 
-    public static final String URI_FILES = "";
     public static final String URI_FILES_KEY = "/{key}";
     
     private final FileStorageService fileStorageService;
 
     @Secured({ CONTRACTOR, BIDDER })
-    @PostMapping(value = URI_FILES, consumes = MULTIPART_FORM_DATA_VALUE)
-    public FileMetadataResponse uploadFile(@RequestParam MultipartFile file)
-            throws IOException {
+    @PostMapping(consumes = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<FileMetadataResponse> uploadFile(@RequestParam MultipartFile file) throws IOException {
         if (file.getOriginalFilename() == null) {
             throw new FileNotExistsException("File is not exists");
         }
-        return fileStorageService.upload(file);
+        return ResponseEntity
+                   .ok(fileStorageService.upload(file));
     }
     
     @Secured({ CONTRACTOR, BIDDER })
--- src/main/java/com/flex/tender/payload/mapper/FileMetadataMapper.java
+++ src/main/java/com/flex/tender/payload/mapper/FileMetadataMapper.java
@@ -1,15 +1,11 @@
 package com.flex.tender.payload.mapper;
 
 import org.mapstruct.Mapper;
-
 import com.flex.tender.model.FileMetadata;
-import com.flex.tender.payload.request.FileMetadataRequest;
 import com.flex.tender.payload.response.FileMetadataResponse;
 
 @Mapper(componentModel = "spring")
 public interface FileMetadataMapper {
-    
-    FileMetadata toEntity(FileMetadataRequest fileMetadataRequest);
     
     FileMetadataResponse toResponse(FileMetadata fileMetadata);
 
