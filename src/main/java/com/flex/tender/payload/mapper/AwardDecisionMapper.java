741d157d81af0bf529a9a94259c6809953f333d3
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-05-21 18:05:35 +0200

    refactored award decision service

--- src/main/java/com/flex/tender/payload/mapper/AwardDecisionMapper.java
+++ src/main/java/com/flex/tender/payload/mapper/AwardDecisionMapper.java
@@ -1,7 +1,7 @@
 package com.flex.tender.payload.mapper;
 
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
 import com.flex.tender.model.AwardDecision;
 import com.flex.tender.payload.request.AwardDecisionRequest;
 import com.flex.tender.payload.response.AwardDecisionResponse;
@@ -9,6 +9,8 @@ import com.flex.tender.payload.response.AwardDecisionResponse;
 @Mapper(componentModel = "spring", uses = FileMetadataMapper.class)
 public interface AwardDecisionMapper {
     
+    @Mapping(target = "tender", ignore = true)
+    @Mapping(target = "fileMetadata", ignore = true)
     AwardDecision toEntity(AwardDecisionRequest awardDecisionRequest);
     
     AwardDecisionResponse toResponse(AwardDecision awardDecision);
--- src/main/java/com/flex/tender/service/impl/AwardDecisionServiceImpl.java
+++ src/main/java/com/flex/tender/service/impl/AwardDecisionServiceImpl.java
@@ -1,29 +1,42 @@
 package com.flex.tender.service.impl;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.flex.tender.model.AwardDecision;
+import com.flex.tender.model.FileMetadata;
 import com.flex.tender.model.Tender;
+import com.flex.tender.payload.mapper.AwardDecisionMapper;
+import com.flex.tender.payload.request.AwardDecisionRequest;
+import com.flex.tender.payload.response.AwardDecisionResponse;
 import com.flex.tender.repository.AwardDecisionRepository;
 import com.flex.tender.service.AwardDecisionService;
-
+import com.flex.tender.service.FileStorageService;
+import com.flex.tender.service.TenderDetailsService;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AwardDecisionServiceImpl implements AwardDecisionService {
 
-    private final AwardDecisionRepository awardRepository;
+    private final AwardDecisionMapper awardDecisionMapper;
+    private final AwardDecisionRepository awardDecisionRepository;
+    private final TenderDetailsService tenderDetailsService;
+    private final FileStorageService fileStorageService;
 
     @Override
-    public AwardDecision save(AwardDecision awardDecision, Tender tender) {
+    @Transactional
+    public AwardDecisionResponse save(AwardDecisionRequest awardDecisionRequest) {
+        AwardDecision awardDecision = awardDecisionMapper.toEntity(awardDecisionRequest);
+        Tender tender = tenderDetailsService.findById(awardDecisionRequest.tenderId());
+        FileMetadata fileMetadata = fileStorageService.findById(awardDecisionRequest.filemetadataId());
         awardDecision.setTender(tender);
-        return awardRepository.save(awardDecision);
+        awardDecision.setFileMetadata(fileMetadata);
+        return awardDecisionMapper.toResponse(awardDecisionRepository.save(awardDecision));
     }
 
     @Override
     public AwardDecision findById(Integer id) {
-        return awardRepository.findById(id);
+        return awardDecisionRepository.findById(id);
     }
 
 }
