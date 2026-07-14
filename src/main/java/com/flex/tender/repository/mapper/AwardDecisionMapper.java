ce46dffe183b0c630ef5e5f94c96c0bc539e95f3
ruslan-timoshchuk <ruslan.timoshchuk.arc@gmail.com>
2026-07-15 00:43:22 +0300

    refactored award decision repository api

--- src/main/java/com/flex/tender/repository/mapper/AwardDecisionMapper.java
+++ src/main/java/com/flex/tender/repository/mapper/AwardDecisionMapper.java
@@ -1,10 +1,8 @@
 package com.flex.tender.repository.mapper;
 
 import static com.flex.tender.repository.sql.column.AwardDecisionColumns.*;
-import static com.flex.tender.repository.mapper.FileMeatadataMapper.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Map;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import com.flex.tender.model.AwardDecision;
@@ -25,11 +23,7 @@ public class AwardDecisionMapper implements RowMapper<AwardDecision> {
         return AwardDecision
                  .builder()
                  .id(resultSet.getInt(AWARD_DECISION_ID))
-                 .fileMetadata(fileMapper.mapFileMetadata(resultSet, 
-                         Map.of(FILE_ID, AWARD_FILE_ID, 
-                                FILE_NAME, AWARD_FILE_NAME, 
-                                FILE_CONTENT_TYPE, AWARD_FILE_CONTENT_TYPE,
-                                FILE_AWS3_KEY, AWARD_FILE_AWS3_KEY)))
+                 .fileMetadata(fileMapper.mapFileMetadata(resultSet))
                  .build();
     }
  
--- src/main/java/com/flex/tender/repository/sql/column/AwardDecisionColumns.java
+++ src/main/java/com/flex/tender/repository/sql/column/AwardDecisionColumns.java
@@ -6,9 +6,5 @@ import lombok.experimental.UtilityClass;
 public class AwardDecisionColumns {
 
     public final String AWARD_DECISION_ID = "award_id";
-    public final String AWARD_FILE_ID = "award_file_id";
-    public final String AWARD_FILE_NAME = "award_file_name";
-    public final String AWARD_FILE_CONTENT_TYPE = "award_file_content_type";
-    public final String AWARD_FILE_AWS3_KEY = "award_aws_s3_file_key";
     
 }
